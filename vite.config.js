1d41f33b41b62e4b8ffefe587ef8f519f607de47
13016063721xin <xin2544067407@qq.com>
2026-04-14 13:02:03 +0800

    6.1

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        // 告诉打包员，咱们有两个入口文件需要打包
+        main: 'index.html',
+        login: 'login_demo.html'
+      }
+    }
+  }
+})
